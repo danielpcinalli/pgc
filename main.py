f0df659f3184f37494ee8109266087cbba60cf5e
danielpcinalli <danielpcinalli@gmail.com>
2020-09-25 12:05:15 -0300

    kfolds para validação cruzada

--- main.py
+++ main.py
@@ -3,54 +3,60 @@ import traceback
 from classificadores import Metodo_acuracia, Metodo_similaridade
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+#from sklearn import cross_validation
+import sklearn
 
 from datasets import load_ecoli, load_anuran, load_yeast
 import util
 
 def main():
     try:
-        '''
+
         numPontosProximos = [10, 30, 50]
-        accMin = 0.9
+        accMin = 0.7
         for npp in numPontosProximos:
             testeMetodoAcuracia(npp, accMin)
+
         '''
         numObjetosGerados = 10
-        for qtdeClassificadores in [5, 15, 80]:
+        for qtdeClassificadores in [5, 15, 25, 80]:
             testeMetodoSimilaridade(numObjetosGerados, qtdeClassificadores)
-
+        '''
         util.alert_sound()
     except:
         traceback.print_exc()
         util.alert_sound()
+
 def testeMetodoAcuracia(numPontosProximos, accMin):
 
     print("MÉTODO: ACURÁCIA")
     print(f"Quantidade de objetos próximos: {numPontosProximos}")
     print(f"Acurácia mínima: {accMin}")
+
     X, y = load_ecoli()
 
+    #clfs = [DecisionTreeClassifier() for i in range(100)]
+    clfs = [RandomForestClassifier(n_estimators = 20) for i in range(50)]
 
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 10)
-    #X_train, X_base, y_train, y_base = train_test_split(X_train, y_train, random_state = 100)
-
-    clfs = [DecisionTreeClassifier() for i in range(100)]
-    #clfs = [RandomForestClassifier(n_estimators = 20) for i in range(50)]
-
-    #macc = Metodo_acuracia(clfs, numPontosProximos, accMin, X_base, y_base)
     macc = Metodo_acuracia(clfs, numPontosProximos, accMin)
 
-    #macc.fit(X_train, y_train)
-    #y_pred = macc.predict(X_test)
+    kf = KFold(n_splits=5, shuffle=True, random_state=1)
 
-    #acc = accuracy_score(y_test, y_pred)
-    #print(f"Acurácia : {acc}")
+    for train_index, test_index in kf.split(X):
 
-    scores = cross_val_score(macc, X, y, cv = 3, scoring = 'accuracy')
-    print(f"Scores : {scores}")
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        macc_clone = sklearn.base.clone(macc)
+
+        macc_clone.fit(X_train, y_train)
+        y_pred = macc_clone.predict(X_test)
+
+        accuracy = accuracy_score(y_pred, y_test)
+        print(accuracy)
 
 def testeMetodoSimilaridade(numObjetosGerados, qtdeClassificadores):
 
@@ -63,19 +69,22 @@ def testeMetodoSimilaridade(numObjetosGerados, qtdeClassificadores):
     #clfs = [DecisionTreeClassifier() for i in range(100)]
     clfs = [RandomForestClassifier(n_estimators = 15) for i in range(80)]
 
-
     msim = Metodo_similaridade(clfs, numObjetosGerados, qtdeClassificadores)
-    #msim.fit(X_train, y_train)
-    #y_pred = msim.predict(X_test)
 
-    scores = cross_val_score(msim, X, y, cv = 3, scoring = 'accuracy')
-    print(f"Scores : {scores}")
+    kf = KFold(n_splits=5, shuffle=True, random_state=1)
 
+    for train_index, test_index in kf.split(X):
 
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
+        msim_clone = sklearn.base.clone(msim)
 
+        msim_clone.fit(X_train, y_train)
+        y_pred = msim_clone.predict(X_test)
 
-
+        accuracy = accuracy_score(y_pred, y_test)
+        print(accuracy)
 
 
 
