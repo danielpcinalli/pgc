d16062c8ac61d229039fdfb9c891a618c89ac6b6
danielpcinalli <danielpcinalli@gmail.com>
2020-10-21 13:55:00 -0300

    novos datasets

--- datasets.py
+++ datasets.py
@@ -22,11 +22,88 @@ FN_PAGE_BLOCKS              = PATH_DATASETS + "page_blocks/page-blocks.data"
 FN_RICE                     = PATH_DATASETS + "rice_gonen_and_jasmine/Rice-Gonen andJasmine.csv"
 FN_LETTER_RECOGNITIION      = PATH_DATASETS + "letter_recognition/letter-recognition.data"
 FN_ELECT_GRID_STABILITY     = PATH_DATASETS + "electrical_grid_stability/Data_for_UCI_named.csv"
+FN_CAR_EVALUATION           = PATH_DATASETS + "car_evaluation/car.data"
+FN_MAGIC_GAMMA_TELESCOPE    = PATH_DATASETS + "MAGIC_gamma_telescope/magic04.data"
+FN_PEN_DIGITS_TS            = PATH_DATASETS + "pen_letter_recognition/pendigits.tes"
+FN_PEN_DIGITS_TR            = PATH_DATASETS + "pen_letter_recognition/pendigits.tra"
+FN_LEAF                     = PATH_DATASETS + "leaf/leaf.csv"
+FN_ACCENT_RECOGNITION       = PATH_DATASETS + "accent_recognition/accent-mfcc-data-1.csv"
+FN_STATLOG_SAT_IMAGE_TR     = PATH_DATASETS + "statlog_sat_image/sat.trn"
+FN_STATLOG_SAT_IMAGE_TS     = PATH_DATASETS + "statlog_sat_image/sat.tst"
+
+
+def load_statlog_sattelite_image(format='np'):
+    data1 = read_csv(FN_STATLOG_SAT_IMAGE_TR, sep = " ", header = None)
+    data2 = read_csv(FN_STATLOG_SAT_IMAGE_TS, sep = " ", header = None)
+
+    data = pd.concat([data1, data2], axis = 0)
+
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    if format == 'np':
+        return X.to_numpy(), y.to_numpy()
+    if format == 'pd':
+        X['class'] = y
+        return X
+
+def load_accent_recognition(format='np'):
+    data = pd.read_csv(FN_ACCENT_RECOGNITION, sep=',')
+
+    X = data.iloc[:, 1:]
+    y = data.iloc[:, 0]
+
+    if format == 'np':
+        return X.to_numpy(), y.to_numpy()
+    if format == 'pd':
+        X['class'] = y
+        return X
+
+
+def load_leaf(format='np'):
+    data = pd.read_csv(FN_LEAF, sep=',', header=None)
+
+    X = data.iloc[:, 2:] # segunda coluna se refere ao espécime
+    y = data.iloc[:, 0]
+
+    if format == 'np':
+        return X.to_numpy(), y.to_numpy()
+    if format == 'pd':
+        X['class'] = y
+        return X
+
+def load_pen_letter_recognition(format='np'):
+    data1 = read_csv(FN_PEN_DIGITS_TS, sep = ",", header = None)
+    data2 = read_csv(FN_PEN_DIGITS_TR, sep = ",", header = None)
+
+    data = pd.concat([data1, data2], axis = 0)
+
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    if format == 'np':
+        return X.to_numpy(), y.to_numpy()
+    if format == 'pd':
+        X['class'] = y
+        return X
+
+def load_gamma_telecope(format='np'):
+
+    data = pd.read_csv(FN_MAGIC_GAMMA_TELESCOPE, sep=',', header=None)
+
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    if format == 'np':
+        return X.to_numpy(), y.to_numpy()
+    if format == 'pd':
+        X['class'] = y
+        return X
 
 def load_yeast(format = 'np'):
     data = pd.read_csv(FN_YEAST, sep = "\s+", header = None)
 
-    X = data.iloc[:, 1:9]
+    X = data.iloc[:, 1:9] # primeira coluna é id
     y = data.iloc[:, 9]
 
 
@@ -227,6 +304,7 @@ def load_electrical_grid_stability(format='np'):
         return X
 
 
+
 def z_score_normalize(X):
     for i in range(X.shape[1]):
         X.iloc[:, i] = (X.iloc[:, i] -X.iloc[:, i].mean())/X.iloc[:, i].std()
@@ -242,9 +320,10 @@ def _test_dataframe():
     return df
 
 def main():
-    df = load_letter_recognition(format='pd')
+    df = load_page_blocks(format='pd')
     print(df.head())
     print(df.dtypes)
+    print(df['class'].value_counts())
 
 if __name__ == '__main__':
     main()
